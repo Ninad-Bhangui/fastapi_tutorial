7a6380d33e4437690a2edc7ddee5c745640293cd
ninadbhangui <ninad.bhangui@aurochssoftware.com>
2020-11-03 23:41:29 +0530

    query params

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
 from enum import Enum
+from typing import Optional
 
 class ModelName(str, Enum):
     alexnet = "alexnet"
     resnet = "resnet"
     lenet = "lenet"
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
 app = FastAPI()
 
@@ -12,9 +15,31 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
+@app.get("/items/")
+async def read_item(skip: int =0, limit: int = 10):
+    return fake_items_db[skip: skip + limit]
+
 @app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
+async def read_item(item_id: int, q: Optional[str] = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"item_id": item_id, "q": q})
+    if not short:
+        item.update({"descripton": "This is an amazing item with a long description"})
+    return item
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, q: Optional[str] = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
 
 @app.get("/users/me")
 async def read_user_me():
